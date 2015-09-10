7d68e56ddd4785e30446681ae746f605c1b06aa9
Student <student@Mac-mini-31.local>
2015-09-10 17:28:47 -0600

    needs to be sylized

---
+++ editinplace.js
@@ -0,0 +1,28 @@
+angular.module("bear", []);
+
+angular.module("bear").controller("polarbearcontroller", ["$scope", function($scope){
+	$scope.justtext=true;
+	$scope.justinput=false;
+	$scope.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
+
+	$scope.Texty = function () {
+		$scope.justtext = false;
+		$scope.justinput = true;
+		
+	}
+
+	 $scope.Edit = function () {
+	 		
+		
+	 }
+
+	 $scope.Update = function () {
+		
+		$scope.justtext = true;
+		$scope.justinput = false;
+	 }
+
+
+}]
+);
