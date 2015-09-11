88c412ef3b961f1d8091f667c3c86036b00cafb4
Student <student@Mac-mini-31.local>
2015-09-11 11:23:21 -0600

    added timeout for focus

--- editinplace.js
+++ editinplace.js
@@ -1,6 +1,6 @@
 angular.module("bear", []);
 
-angular.module("bear").controller("polarbearcontroller", ["$scope", function($scope){
+angular.module("bear").controller("polarbearcontroller", ["$scope", "$timeout", function($scope, $timeout){
 	$scope.justtext=true;
 	$scope.justinput=false;
 	$scope.message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -9,14 +9,11 @@ angular.module("bear").controller("polarbearcontroller", ["$scope", function($sc
 	$scope.Texty = function () {
 		$scope.justtext = false;
 		$scope.justinput = true;
-		
+		$timeout( function() {document.getElementById("foc").focus();
+		},0);
 	}
 
-	 $scope.Edit = function () {
-	 		
-		
-	 }
-
+	
 	 $scope.Update = function () {
 		
 		$scope.justtext = true;
